9ea458bac52e9ee6338e33dcaab0f2d60dddd150
mvernon27 <mvernon2705@gmail.com>
2019-10-01 23:00:08 -0400

    Rebuild

--- FrontEnd/dist/app.c328ef1a.js
+++ FrontEnd/dist/app.c328ef1a.js
@@ -1067,7 +1067,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50879" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52510" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
